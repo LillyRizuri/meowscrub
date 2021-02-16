1016616d0db256f5850b916606fb3d2a85ccf729
scrubthispie <minh99460@gmail.com>
2021-02-16 22:19:24 +0700

    Changed how the 8ball command replies.

--- cmds/funs/8ball.js
+++ cmds/funs/8ball.js
@@ -5,26 +5,26 @@ const Discord = require('discord.js')
 const { embedcolor } = require('../../colors.json')
 
 const responses = [
-    "<:scrubgreen:797476323316465676> It is certain.",
-    "<:scrubgreen:797476323316465676> It is decidedly so.",
-    "<:scrubgreen:797476323316465676> Without a doubt.",
-    "<:scrubgreen:797476323316465676> Yes – definitely.",
-    "<:scrubgreen:797476323316465676> You may rely on it.",
-    "<:scrubgreen:797476323316465676> As I see it, yes.",
-    "<:scrubgreen:797476323316465676> Most likely.",
-    "<:scrubgreen:797476323316465676> Outlook good.",
-    "<:scrubgreen:797476323316465676> Yes.",
-    "<:scrubgreen:797476323316465676> Signs point to yes.",
-    "<:scrubyellow:797816476912058388> Reply hazy, try again.",
-    "<:scrubyellow:797816476912058388> Ask again later.",
-    "<:scrubyellow:797816476912058388> Better not tell you now.",
-    "<:scrubyellow:797816476912058388> Cannot predict now.",
-    "<:scrubyellow:797816476912058388> Concentrate and ask again.",
-    "<:scrubred:797476323169533963> Don’t count on it.",
-    "<:scrubred:797476323169533963> My reply is no.",
-    "<:scrubred:797476323169533963> My sources say no.",
-    "<:scrubred:797476323169533963> Outlook not so good.",
-    "<:scrubred:797476323169533963> Very doubtful.",
+    "It is certain",
+    "It is decidedly so",
+    "Without a doubt",
+    "Yes – definitely",
+    "You may rely on it",
+    "As I see it, yes",
+    "Most likely",
+    "Outlook good",
+    "Yes",
+    "Signs point to yes",
+    "Reply hazy, try again",
+    "Ask again later",
+    "Better not tell you now",
+    "Cannot predict now",
+    "Concentrate and ask again",
+    "Don’t count on it",
+    "My reply is no",
+    "My sources say no",
+    "Outlook not so good",
+    "Very doubtful",
 ]
 
 module.exports = class rpsCommand extends Commando.Command {
@@ -46,12 +46,6 @@ module.exports = class rpsCommand extends Commando.Command {
         }
 
         const answer = responses[Math.floor(Math.random() * (responses.length))]
-        const eightBallEmbed = new Discord.MessageEmbed()
-            .setColor(embedcolor)
-            .setAuthor('The Magic 8-Ball')
-            .addField(`Your question: ${question}`, `Answer: ${answer}`)
-            .setFooter('hope it helps')
-            .setTimestamp()
-        message.channel.send(eightBallEmbed)
+        message.channel.send(`🎱 | ${answer}, **${message.author.username}**.`)
     }
 }
