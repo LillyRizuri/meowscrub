6b925f432272cfb09c8d9d32467ef259efa05b48
scrubthispie <minh99460@gmail.com>
2021-02-19 00:25:24 +0700

    Removed the timer for snipe because it's not suitable

--- events/msg-snipe.js
+++ events/msg-snipe.js
@@ -2,9 +2,8 @@ const fs = require("fs");
 const snipe = require("../snipe.json")
 module.exports = {
     run(message, args, client, author, tag, time, icon) {
-        let content = args;
-
-        let snipedMsg = content.join(" ");
+        let content = args
+        let snipedMsg = content.join(" ")
         snipe[message.channel.id] = {
             msg: snipedMsg,
             user: author,
@@ -16,9 +15,5 @@ module.exports = {
         // To log deleted messages into snipe.json
         // let snipez = JSON.stringify(snipe)
         // fs.writeFile("./snipe.json", snipez, (err) => console.error) 
-        
-        setTimeout(function () {
-            delete(snipe[message.channel.id])
-        }, 60000)
     }
 }
