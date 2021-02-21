d5ec7abed2e97c072242f89bd0f5a46c428a0baa
scrubthispie <minh99460@gmail.com>
2021-02-21 15:11:38 +0700

    Updated avatar and whois command to accept user outside the guild it was ran on with ID.

--- cmds/utility/avatar.js
+++ cmds/utility/avatar.js
@@ -12,40 +12,55 @@ module.exports = class AvatarCommand extends Commando.Command {
             memberName: 'avatar',
             description: "Return your/someone else's avatar.",
             argsType: 'single',
-            format: '[@user]',
-            examples: ['avatar', 'avatar @frockles'],
+            format: '[@user/userID]',
+            examples: ['avatar', 'avatar @frockles', 'avatar 693832549943869493'],
             guildOnly: true
         })
     }
 
     run(message, args) {
         let user
+        let avatar
 
-        try {
-            if (message.mentions.users.first()) {
-                user = message.mentions.users.first()
-            } else if (args) {
-                user = message.guild.members.cache.get(args).user
-            } else {
-                user = message.author
-            }
-
-            const avatar = user.displayAvatarURL({ format: 'png', size: 4096, dynamic: true })
-
+        if (message.mentions.users.first()) {
+            user = message.mentions.users.first()
+            avatar = user.displayAvatarURL({ format: 'png', size: 4096, dynamic: true })
             const avatarEmbed = new Discord.MessageEmbed()
                 .setColor(embedcolor)
                 .setAuthor(`${user.tag}'s Profile Picture`)
                 .setImage(avatar)
                 .setFooter(`Requested by ${message.author.tag}`, message.author.displayAvatarURL({ dynamic: true }))
             message.channel.send(avatarEmbed)
-        } catch (err) {
-            const notValidIDEmbed = new Discord.MessageEmbed()
-                .setColor(red)
-                .setDescription('<:scrubred:797476323169533963> What are you trying to do with that invalid user ID?')
-                .setFooter('you fool')
-                .setTimestamp()
-            message.reply(notValidIDEmbed)
             return
+        } else if (args) {
+            this.client.users.fetch(args)
+                .then(async user => {
+                    avatar = user.displayAvatarURL({ format: 'png', size: 4096, dynamic: true })
+                    const avatarEmbed = new Discord.MessageEmbed()
+                        .setColor(embedcolor)
+                        .setAuthor(`${user.tag}'s Profile Picture`)
+                        .setImage(avatar)
+                        .setFooter(`Requested by ${message.author.tag}`, message.author.displayAvatarURL({ dynamic: true }))
+                    message.channel.send(avatarEmbed)
+                    return
+                }).catch(err => {
+                    const notValidIDEmbed = new Discord.MessageEmbed()
+                        .setColor(red)
+                        .setDescription('<:scrubred:797476323169533963> What are you trying to do with that invalid user ID?')
+                        .setFooter('you fool')
+                        .setTimestamp()
+                    message.reply(notValidIDEmbed)
+                    return
+                })
+        } else {
+            user = message.author
+            avatar = user.displayAvatarURL({ format: 'png', size: 4096, dynamic: true })
+            const avatarEmbed = new Discord.MessageEmbed()
+                .setColor(embedcolor)
+                .setAuthor(`${user.tag}'s Profile Picture`)
+                .setImage(avatar)
+                .setFooter(`Requested by ${message.author.tag}`, message.author.displayAvatarURL({ dynamic: true }))
+            message.channel.send(avatarEmbed)
         }
     }
 }
--- cmds/utility/whois.js
+++ cmds/utility/whois.js
@@ -4,6 +4,12 @@ const moment = require('moment')
 
 const { red, embedcolor } = require('../../colors.json')
 
+const notValidIDEmbed = new Discord.MessageEmbed()
+    .setColor(red)
+    .setDescription('<:scrubred:797476323169533963> What are you trying to do with that invalid user ID?')
+    .setFooter('you fool')
+    .setTimestamp()
+
 module.exports = class WhoIsCommand extends Commando.Command {
     constructor(client) {
         super(client, {
@@ -20,47 +26,51 @@ module.exports = class WhoIsCommand extends Commando.Command {
     }
 
     async run(message, args) {
-        try {
-            const { guild, channel } = message
+        // worst code ever
+        let guild = this.client.guilds.cache.get(message.guild.id)
+        let user
+        let member
+        let rolemap
+        let thenJoin
+        let timeJoin
+        let joinedAt
+        let joinedaAtHM
+        let thenRegister
+        let timeRegister
+        let registeredAt
+        let registeredAtHM
 
-            let user
+        if (message.mentions.users.first()) {
+            // if the input was an user mention
+            user = message.mentions.users.first()
+            member = message.guild.members.cache.get(user.id)
 
-            if (message.mentions.users.first()) {
-                user = message.mentions.users.first()
-            } else if (args[0]) {
-                user = message.guild.members.cache.get(args).user
-            } else {
-                user = message.author
-            }
-
-            const member = guild.members.cache.get(user.id)
-
-            let rolemap = member.roles.cache
+            rolemap = member.roles.cache
                 .sort((a, b) => b.position - a.position)
                 .map(r => r)
                 .join(' ')
             if (rolemap.length > 800) rolemap = 'Too many roles to display.'
             if (!rolemap) rolemap = 'No roles.'
 
-            const thenJoin = moment(member.joinedTimestamp)
-            const timeJoin = thenJoin.from(moment())
-            const joinedAt = thenJoin.format("MMM Do, YYYY")
-            const joinedaAtHM = thenJoin.format("HH:MM")
+            thenJoin = moment(member.joinedTimestamp)
+            timeJoin = thenJoin.from(moment())
+            joinedAt = thenJoin.format("MMM Do, YYYY")
+            joinedaAtHM = thenJoin.format("HH:MM")
 
-            const thenRegister = moment(user.createdTimestamp)
-            const timeRegister = thenRegister.from(moment())
-            const RegisteredAt = thenRegister.format("MMM Do, YYYY")
-            const RegisteredAtHM = thenRegister.format("HH:MM")
+            thenRegister = moment(user.createdTimestamp)
+            timeRegister = thenRegister.from(moment())
+            registeredAt = thenRegister.format("MMM Do, YYYY")
+            registeredAtHM = thenRegister.format("HH:MM")
 
             const infoEmbed = new Discord.MessageEmbed()
                 .setColor(embedcolor)
-                .setAuthor(`Information for ${user.username}` , user.displayAvatarURL())
+                .setAuthor(`Information for ${user.username}`, user.displayAvatarURL())
                 .setThumbnail(user.displayAvatarURL())
                 .setDescription(`[<@${user.id}>]`)
                 .addFields({
                     name: 'Member Details',
                     value:
-`
+                        `
 • Nickname: \`${member.nickname || 'None'}\`
 • Roles [${member.roles.cache.size}]: ${rolemap}            
 • Joined: \`${joinedAt} ${joinedaAtHM} (${timeJoin})\`
@@ -69,25 +79,156 @@ module.exports = class WhoIsCommand extends Commando.Command {
                 }, {
                     name: 'User Details',
                     value:
-`
+                        `
 • ID: \`${user.id}\`
 • Username: \`${user.tag}\`
-• Created: \`${RegisteredAt} ${RegisteredAtHM} (${timeRegister})\`
+• Created: \`${registeredAt} ${registeredAtHM} (${timeRegister})\`
 • Status: \`${(user.presence.status).replace('dnd', 'Do Not Disturb').toProperCase()}\`   
 • Is Bot: \`${(user.bot).toString().replace('true', 'Yes').replace('false', 'No')}\`
 `
                 })
                 .setFooter(`Requested by ${message.author.tag}`, message.author.displayAvatarURL({ dynamic: true }))
                 .setTimestamp()
-            channel.send(infoEmbed)
-        } catch (err) {
-            const notValidIDEmbed = new Discord.MessageEmbed()
-                .setColor(red)
-                .setDescription('<:scrubred:797476323169533963> What are you trying to do with that invalid user ID?')
-                .setFooter('you fool')
-                .setTimestamp()
-            return message.reply(notValidIDEmbed)
+            message.channel.send(infoEmbed)
+        } else if (args[0]) {
+            // if the input was a string
+            if (!guild.member(args)) {
+                // check if a person isn't in the guild it was ran on
+                this.client.users.fetch(args)
+                    .then(async user => {
+                        member = message.guild.members.cache.get(user.id)
 
+                        thenRegister = moment(user.createdTimestamp)
+                        timeRegister = thenRegister.from(moment())
+                        registeredAt = thenRegister.format("MMM Do, YYYY")
+                        registeredAtHM = thenRegister.format("HH:MM")
+
+                        const infoEmbed = new Discord.MessageEmbed()
+                            .setColor(embedcolor)
+                            .setAuthor(`Information for ${user.username}`, user.displayAvatarURL())
+                            .setThumbnail(user.displayAvatarURL())
+                            .setDescription(`[<@${user.id}>]`)
+                            .addFields({
+                                name: 'User Details',
+                                value:
+                                    `
+• ID: \`${user.id}\`
+• Username: \`${user.tag}\`
+• Created: \`${registeredAt} ${registeredAtHM} (${timeRegister})\`
+• Is Bot: \`${(user.bot).toString().replace('true', 'Yes').replace('false', 'No')}\`
+`
+                            })
+                            .setFooter(`Requested by ${message.author.tag}`, message.author.displayAvatarURL({ dynamic: true }))
+                            .setTimestamp()
+                        message.channel.send(infoEmbed)
+                    }).catch(err => {
+                        message.reply(notValidIDEmbed)
+                    })
+            } else {
+                // it's the reverse
+                try {
+                    user = message.guild.members.cache.get(args).user
+                    member = message.guild.members.cache.get(user.id)
+
+                    rolemap = member.roles.cache
+                        .sort((a, b) => b.position - a.position)
+                        .map(r => r)
+                        .join(' ')
+                    if (rolemap.length > 800) rolemap = 'Too many roles to display.'
+                    if (!rolemap) rolemap = 'No roles.'
+
+                    thenJoin = moment(member.joinedTimestamp)
+                    timeJoin = thenJoin.from(moment())
+                    joinedAt = thenJoin.format("MMM Do, YYYY")
+                    joinedaAtHM = thenJoin.format("HH:MM")
+
+                    thenRegister = moment(user.createdTimestamp)
+                    timeRegister = thenRegister.from(moment())
+                    registeredAt = thenRegister.format("MMM Do, YYYY")
+                    registeredAtHM = thenRegister.format("HH:MM")
+
+                    const infoEmbed = new Discord.MessageEmbed()
+                        .setColor(embedcolor)
+                        .setAuthor(`Information for ${user.username}`, user.displayAvatarURL())
+                        .setThumbnail(user.displayAvatarURL())
+                        .setDescription(`[<@${user.id}>]`)
+                        .addFields({
+                            name: 'Member Details',
+                            value:
+                                `
+• Nickname: \`${member.nickname || 'None'}\`
+• Roles [${member.roles.cache.size}]: ${rolemap}            
+• Joined: \`${joinedAt} ${joinedaAtHM} (${timeJoin})\`
+• Activity: \`${user.presence.activities[0] ? user.presence.activities[0].name : 'None'}\`
+                    `
+                        }, {
+                            name: 'User Details',
+                            value:
+                                `
+• ID: \`${user.id}\`
+• Username: \`${user.tag}\`
+• Created: \`${registeredAt} ${registeredAtHM} (${timeRegister})\`
+• Status: \`${(user.presence.status).replace('dnd', 'Do Not Disturb').toProperCase()}\`   
+• Is Bot: \`${(user.bot).toString().replace('true', 'Yes').replace('false', 'No')}\`
+`
+                        })
+                        .setFooter(`Requested by ${message.author.tag}`, message.author.displayAvatarURL({ dynamic: true }))
+                        .setTimestamp()
+                    message.channel.send(infoEmbed)
+                } catch (err) {
+                    message.reply(notValidIDEmbed)
+                }
+            }
+        } else {
+            // if there's no argument, check for the user who ran the command
+            user = message.author
+            member = guild.members.cache.get(user.id)
+
+            rolemap = member.roles.cache
+                .sort((a, b) => b.position - a.position)
+                .map(r => r)
+                .join(' ')
+            if (rolemap.length > 800) rolemap = 'Too many roles to display.'
+            if (!rolemap) rolemap = 'No roles.'
+
+            thenJoin = moment(member.joinedTimestamp)
+            timeJoin = thenJoin.from(moment())
+            joinedAt = thenJoin.format("MMM Do, YYYY")
+            joinedaAtHM = thenJoin.format("HH:MM")
+
+            thenRegister = moment(user.createdTimestamp)
+            timeRegister = thenRegister.from(moment())
+            registeredAt = thenRegister.format("MMM Do, YYYY")
+            registeredAtHM = thenRegister.format("HH:MM")
+
+            const infoEmbed = new Discord.MessageEmbed()
+                .setColor(embedcolor)
+                .setAuthor(`Information for ${user.username}`, user.displayAvatarURL())
+                .setThumbnail(user.displayAvatarURL())
+                .setDescription(`[<@${user.id}>]`)
+                .addFields({
+                    name: 'Member Details',
+                    value:
+                        `
+• Nickname: \`${member.nickname || 'None'}\`
+• Roles [${member.roles.cache.size}]: ${rolemap}            
+• Joined: \`${joinedAt} ${joinedaAtHM} (${timeJoin})\`
+• Activity: \`${user.presence.activities[0] ? user.presence.activities[0].name : 'None'}\`
+                    `
+                }, {
+                    name: 'User Details',
+                    value:
+                        `
+• ID: \`${user.id}\`
+• Username: \`${user.tag}\`
+• Created: \`${registeredAt} ${registeredAtHM} (${timeRegister})\`
+• Status: \`${(user.presence.status).replace('dnd', 'Do Not Disturb').toProperCase()}\`   
+• Is Bot: \`${(user.bot).toString().replace('true', 'Yes').replace('false', 'No')}\`
+`
+                })
+                .setFooter(`Requested by ${message.author.tag}`, message.author.displayAvatarURL({ dynamic: true }))
+                .setTimestamp()
+            message.channel.send(infoEmbed)
         }
     }
 }
