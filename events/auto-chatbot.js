bfd0cb2d943bcf9207989b1b2294e4aa2cc33ca1
scrubthispie <minh99460@gmail.com>
2021-03-07 19:26:28 +0700

    and more

--- events/auto-chatbot.js
+++ events/auto-chatbot.js
@@ -1,8 +1,10 @@
 const fetch = require('node-fetch')
+const utf8 = require('utf8')
 const settingsSchema = require('../schemas/settings-schema')
 const mongo = require('../mongo')
 
 module.exports = async (client, message) => {
+    let channel
     const guildId = message.guild.id
     const input = encodeURIComponent(message.content)
 
@@ -13,18 +15,18 @@ module.exports = async (client, message) => {
                     guildId
                 })
 
-                if (results) {
-                    for (let i = 0; i < results.length; i++) {
-                        let { chatbotChannel } = results[i]
-                        if (message.author.bot) return
-                        if (chatbotChannel.includes(message.channel.id)) {
-                            message.channel.startTyping()
-                            const response = await fetch(`${process.env.BRAINSHOP}&uid=1&msg=${input}`)
-                            const json = await response.json()
-                            message.channel.send(json.cnt.toLowerCase())
-                            return message.channel.stopTyping(true)
-                        }
-                    }
+                for (let i = 0; i < results.length; i++) {
+                    let { chatbotChannel } = results[i]
+                    channel = chatbotChannel
+                }
+
+                if (message.author.bot) return
+                if (channel.includes(message.channel.id)) {
+                    message.channel.startTyping()
+                    const response = await fetch(utf8.encode(`http://api.brainshop.ai/get?bid=${process.env.BRAINSHOP_BRAIN_ID}&key=${process.env.BRAINSHOP_API_KEY}&uid=${message.author.id}&msg=${input}`))
+                    const json = await response.json()
+                    message.channel.send(json.cnt.toLowerCase())
+                    return message.channel.stopTyping(true)
                 }
             } finally {
                 mongoose.connection.close()
