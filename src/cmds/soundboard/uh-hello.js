691d78fa637858c580e4796d7538b80999d8b03b
scrubthispie <minh99460@gmail.com>
2021-04-30 22:12:13 +0700

    added some more sounds to soundboard

---
+++ src/cmds/soundboard/uh-hello.js
@@ -0,0 +1,37 @@
+const Commando = require("discord.js-commando");
+
+module.exports = class PlayAudioCommand extends Commando.Command {
+  constructor(client) {
+    super(client, {
+      name: "uh-hello",
+      group: "soundboard",
+      memberName: "uh-hello",
+      description: "Uh, hello?",
+      throttling: {
+        usages: 1,
+        duration: 5,
+      },
+      guildOnly: true,
+    });
+  }
+
+  async run(message) {
+    const { voice } = message.member;
+    const voiceChannel = message.member.voice.channel;
+
+    if (!voice.channelID) {
+      message.reply(
+        "<:scrubred:797476323169533963> Join an appropriate voice channel for shizzles. Now."
+      );
+      return;
+    }
+
+    voice.channel.join().then((connection) => {
+      connection.play("./src/assets/ogg/uh-hello.ogg").on("finish", () => {
+        voiceChannel.leave();
+      });
+    });
+
+    await message.react("🔊");
+  }
+};
