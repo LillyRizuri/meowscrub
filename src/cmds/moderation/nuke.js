6f750a9973c4be36f49c0265ed7eb02e0ee8a111
scrubthispie <minh99460@gmail.com>
2021-06-13 13:41:33 +0700

    make the nuke command so that people won't do a mistake not on purpose

--- src/cmds/moderation/nuke.js
+++ src/cmds/moderation/nuke.js
@@ -2,8 +2,6 @@ const Commando = require("discord.js-commando");
 const Discord = require("discord.js");
 
 const { green, embedcolor } = require("../../assets/json/colors.json");
-const checkMark = "<:scrubgreenlarge:797816509967368213>";
-const cross = "<:scrubredlarge:797816510579998730>";
 
 module.exports = class NukeCommand extends Commando.Command {
   constructor(client) {
@@ -57,51 +55,97 @@ module.exports = class NukeCommand extends Commando.Command {
         message.author.displayAvatarURL({ dynamic: true })
       ).setDescription(`
 You will attempt to nuke this channel: [${channelToNuke}]
-**Please confirm with a check mark or with a red cross.**
-**Do this action at your own risk.**
+**+ Please confirm by typing "CONFIRM" in all caps.**
+**+ Type anything beside "CONFIRM" (in all caps) to cancel the operation.**
+**+ Do this action at your own risk.**
       `);
-    const msg = await message.reply(confirmationEmbed);
-    await msg.react(checkMark);
-    await msg.react(cross);
+    await message.reply(confirmationEmbed);
 
-    const filter = (reaction, user) =>
-      user.id == message.author.id &&
-      (reaction.emoji.name == "scrubgreenlarge" ||
-        reaction.emoji.name == "scrubredlarge");
+    const filter = (m) => m.author === message.author;
 
-    msg
-      .awaitReactions(filter, { max: 1, time: 30000 })
-      .then(async (reactionCollected) => {
-        if (reactionCollected.first().emoji.name == "scrubgreenlarge") {
+    message.channel
+      .awaitMessages(filter, { max: 1, time: 30000 })
+      .then(async (collected) => {
+        if (collected.first().content === "CONFIRM") {
           try {
             await message.channel.send(
               "Right. The timer for 10 seconds has been set for the nuke."
             );
           } finally {
             setTimeout(async () => {
-              const newChannel = await channelToNuke.clone({
-                reason: `Operation done by ${message.author.tag}`,
-              });
+              try {
+                const newChannel = await channelToNuke.clone({
+                  reason: `Operation done by ${message.author.tag}`,
+                });
 
-              await channelToNuke.delete();
-              const nukeCompleteEmbed = new Discord.MessageEmbed()
-                .setColor(green)
-                .setDescription(
-                  "<:scrubgreen:797476323316465676> **Successfully nuked the following channel.**"
-                )
-                .setFooter("what do you want me to do?")
-                .setTimestamp();
-              if (channelToNuke !== message.channel)
-                await message.channel.send(nukeCompleteEmbed);
-              await newChannel.send(nukeCompleteEmbed);
+                await channelToNuke.delete();
+                const nukeCompleteEmbed = new Discord.MessageEmbed()
+                  .setColor(green)
+                  .setDescription(
+                    "<:scrubgreen:797476323316465676> **Successfully nuked the following channel.**"
+                  )
+                  .setFooter("what do you want me to do?")
+                  .setTimestamp();
+                if (channelToNuke !== message.channel)
+                  await message.channel.send(nukeCompleteEmbed);
+                await newChannel.send(nukeCompleteEmbed);
+              } catch (err) {
+                message.channel.send(
+                  `An unexpected error occured. Maybe it's due to I don't have sufficient permission to delete/clone the target channel.\n\`${err}\``
+                );
+              }
             }, 10000);
           }
-        } else message.channel.send("Operation canceled. Phew.");
+        } else {
+          return message.channel.send("Operation canceled. Phew.");
+        }
       })
       .catch(() => {
         message.channel.send(
-          "No reaction after 30 seconds, operation canceled."
+          "No message confirmation after 30 seconds, operation canceled."
         );
       });
+    // await msg.react(checkMark);
+    // await msg.react(cross);
+
+    // const filter = (reaction, user) =>
+    //   user.id == message.author.id &&
+    //   (reaction.emoji.name == "scrubgreenlarge" ||
+    //     reaction.emoji.name == "scrubredlarge");
+
+    // msg
+    //   .awaitReactions(filter, { max: 1, time: 30000 })
+    //   .then(async (reactionCollected) => {
+    //     if (reactionCollected.first().emoji.name == "scrubgreenlarge") {
+    //       try {
+    //         await message.channel.send(
+    //           "Right. The timer for 10 seconds has been set for the nuke."
+    //         );
+    //       } finally {
+    //         setTimeout(async () => {
+    //           const newChannel = await channelToNuke.clone({
+    //             reason: `Operation done by ${message.author.tag}`,
+    //           });
+
+    //           await channelToNuke.delete();
+    //           const nukeCompleteEmbed = new Discord.MessageEmbed()
+    //             .setColor(green)
+    //             .setDescription(
+    //               "<:scrubgreen:797476323316465676> **Successfully nuked the following channel.**"
+    //             )
+    //             .setFooter("what do you want me to do?")
+    //             .setTimestamp();
+    //           if (channelToNuke !== message.channel)
+    //             await message.channel.send(nukeCompleteEmbed);
+    //           await newChannel.send(nukeCompleteEmbed);
+    //         }, 10000);
+    //       }
+    //     } else message.channel.send("Operation canceled. Phew.");
+    //   })
+    //   .catch(() => {
+    //     message.channel.send(
+    //       "No reaction after 30 seconds, operation canceled."
+    //     );
+    //   });
   }
 };
