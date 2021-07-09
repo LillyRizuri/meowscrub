fa35554acf88eee3fad591f2cec7bf76bd5ca9ba
scrubthispie <minh99460@gmail.com>
2021-07-09 14:16:33 +0700

    improved url checking regex

--- src/modules.js
+++ src/modules.js
@@ -41,7 +41,7 @@ module.exports.urlify = (string) => {
   //   "i"
   // ); // fragment locator
 
-  const pattern = /^((ftp|http|https):\/\/)?www\.([A-z]+)\.([A-z]{2,})/;
+  const pattern = /^((ftp|http|https):\/\/)?(www\.)?([A-z]+)\.([A-z]{2,})/;
 
   return string.replace(pattern, function (url) {
     // eslint-disable-next-line quotes
