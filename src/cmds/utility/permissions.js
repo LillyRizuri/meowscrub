76516425894f01ccadb330c766e61181493a0116
scrubthispie <minh99460@gmail.com>
2021-07-11 10:39:45 +0700

    updated the permissions command

--- src/cmds/utility/permissions.js
+++ src/cmds/utility/permissions.js
@@ -1,47 +1,8 @@
 const Commando = require("discord.js-commando");
 const Discord = require("discord.js");
 
-const yes = "✔️";
-const no = "❌";
-const x = "```";
-const s = "📛";
-const c = "♨️";
-
-const permissions = [
-  "CREATE_INSTANT_INVITE",
-  "KICK_MEMBERS",
-  "BAN_MEMBERS",
-  "ADMINISTRATOR",
-  "MANAGE_CHANNELS",
-  "MANAGE_GUILD",
-  "ADD_REACTIONS",
-  "VIEW_AUDIT_LOG",
-  "PRIORITY_SPEAKER",
-  "STREAM",
-  "VIEW_CHANNEL",
-  "SEND_MESSAGES",
-  "SEND_TTS_MESSAGES",
-  "MANAGE_MESSAGES",
-  "EMBED_LINKS",
-  "ATTACH_FILES",
-  "READ_MESSAGE_HISTORY",
-  "MENTION_EVERYONE",
-  "USE_EXTERNAL_EMOJIS",
-  "VIEW_GUILD_INSIGHTS",
-  "CONNECT",
-  "SPEAK",
-  "MUTE_MEMBERS",
-  "DEAFEN_MEMBERS",
-  "MOVE_MEMBERS",
-  "USE_VAD",
-  "CHANGE_NICKNAME",
-  "MANAGE_NICKNAMES",
-  "MANAGE_ROLES",
-  "MANAGE_WEBHOOKS",
-  "MANAGE_EMOJIS",
-];
-
-const { embedcolor } = require("../../assets/json/colors.json");
+const modPerms = require("../../assets/json/mod-permissions.json");
+const normalPerms = require("../../assets/json/normal-permissions.json");
 
 module.exports = class UserPermissionsCommand extends Commando.Command {
   constructor(client) {
@@ -51,8 +12,8 @@ module.exports = class UserPermissionsCommand extends Commando.Command {
       group: "utility",
       memberName: "permissions",
       description: "Shows your, or a specified user's permission.",
-      argsType: "single",
-      format: "[@user/userID]",
+      argsType: "multiple",
+      format: "<@user/userID> [--text]",
       examples: ["permissions @frockles"],
       clientPermissions: ["EMBED_LINKS"],
       throttling: {
@@ -63,36 +24,73 @@ module.exports = class UserPermissionsCommand extends Commando.Command {
     });
   }
 
-  run(message, args) {
-    let user;
+  async run(message, args) {
+    if (!args[0])
+      return message.reply(
+        "<:scrubnull:797476323533783050> Please provide an user mention/user ID in order to continue."
+      );
 
+    let target;
     try {
-      if (message.mentions.users.first()) {
-        user = message.mentions.users.first();
-      } else if (args) {
-        user = message.guild.members.cache.get(args).user;
-      } else {
-        user = message.author;
-      }
-
-      const member = message.guild.members.cache.get(user.id);
-
-      let description = `Server - ${s}\nCurrent Channel - ${c}\n\n${s} | ${c}\n`;
-
-      const permListEmbed = new Discord.MessageEmbed()
-        .setColor(embedcolor)
-        .setAuthor(`${user.tag}'s Permissions`);
-      permissions.forEach((perm) => {
-        description += `${member.permissions.has(perm) ? yes : no} | ${
-          message.channel.permissionsFor(user.id).has(perm) ? yes : no
-        } - ${perm.split("_").join(" ").toProperCase()}\n`;
-      });
-      permListEmbed.setDescription(x + description + x);
-      message.channel.send(permListEmbed);
+      target =
+        message.mentions.users.first() ||
+        (await this.client.users.fetch(args[0]));
     } catch (err) {
-      message.reply(
+      return message.reply(
         "<:scrubred:797476323169533963> What are you trying to do with that invalid user ID?"
       );
     }
+
+    const member = message.guild.members.cache.get(target.id);
+
+    let authorText = "";
+    let modPermsDesc = "";
+    let normalPermsDesc = "";
+
+    if (args[1] && args[1].toLowerCase() === "--text") {
+      authorText = `${target.tag}'s Current Text Channel Permissions`;
+      modPerms.forEach((perm) => {
+        modPermsDesc += `${
+          message.channel.permissionsFor(target.id).has(perm) ? "✅" : "❌"
+        } | ${perm.split("_").join(" ").toProperCase()}\n`;
+      });
+
+      normalPerms.forEach((perm) => {
+        normalPermsDesc += `${
+          message.channel.permissionsFor(target.id).has(perm) ? "✅" : "❌"
+        } | ${perm.split("_").join(" ").toProperCase()}\n`;
+      });
+    } else {
+      authorText = `${target.tag}'s Current Server Permissions`;
+      modPerms.forEach((perm) => {
+        modPermsDesc += `${member.permissions.has(perm) ? "✅" : "❌"} | ${perm
+          .split("_")
+          .join(" ")
+          .toProperCase()}\n`;
+      });
+
+      normalPerms.forEach((perm) => {
+        normalPermsDesc += `${
+          member.permissions.has(perm) ? "✅" : "❌"
+        } | ${perm.split("_").join(" ").toProperCase()}\n`;
+      });
+    }
+
+    const permListEmbed = new Discord.MessageEmbed()
+      .setColor(member.displayHexColor)
+      .setAuthor(authorText, target.displayAvatarURL())
+      .addFields(
+        {
+          name: "Moderation",
+          value: `\`\`\`\n${modPermsDesc}\n\`\`\``,
+          inline: true,
+        },
+        {
+          name: "Texting",
+          value: `\`\`\`\n${normalPermsDesc}\n\`\`\``,
+          inline: true,
+        }
+      );
+    message.channel.send(permListEmbed);
   }
 };
