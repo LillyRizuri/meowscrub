42106a8b3789849f4ad0557661e96423a4df8b89
scrubthispie <minh99460@gmail.com>
2021-07-30 11:51:59 +0700

    updated vote command

--- src/cmds/conventional/vote.js
+++ src/cmds/conventional/vote.js
@@ -31,13 +31,10 @@ module.exports = class VoteCommand extends Commando.Command {
 
     switch (resJson.voted) {
       case 0:
-        voteDesc = `You haven't voted for **${this.client.user.username}** yet!`;
-        break;
-      case 1:
-        voteDesc = `You've voted for **${this.client.user.username}** \`${resJson.voted}\` time!`;
+        voteDesc = `You haven't voted for **${this.client.user.username}** yet!\n[Click here to vote](https://top.gg/bot/${this.client.user.id}/vote)`;
         break;
       default:
-        voteDesc = `You've voted for **${this.client.user.username}** \`${resJson.voted}\` times!`;
+        voteDesc = `You've voted for **${this.client.user.username}** already!`;
         break;
     }
 
@@ -47,9 +44,7 @@ module.exports = class VoteCommand extends Commando.Command {
         `Vote for ${this.client.user.username}`,
         this.client.user.displayAvatarURL()
       )
-      .setDescription(
-        `${voteDesc}\n[Vote for ${this.client.user.username}](https://top.gg/bot/${this.client.user.id}/vote)`
-      );
+      .setDescription(voteDesc);
 
     message.channel.send(voteEmbed);
   }
