a33afec81c8642b43ce45b65a4b0c0aca9b88689
scrubthispie <minh99460@gmail.com>
2021-09-13 14:39:44 +0700

    some random change

--- src/events/client-presence.js
+++ src/events/client-presence.js
@@ -2,10 +2,20 @@ module.exports = {
   name: "ready",
   async execute(client) {
     // first presence status before the interval starts
+    // client.user.setPresence({
+    //   activities: [
+    //     {
+    //       name: `discord.js doing its thing | @${client.user.username} help`,
+    //       type: "WATCHING",
+    //     },
+    //   ],
+    //   status: "idle",
+    // });
+
     client.user.setPresence({
       activities: [
         {
-          name: `discord.js doing its thing | @${client.user.username} help`,
+          name: `me waiting for verification | @${client.user.username} help`,
           type: "WATCHING",
         },
       ],
@@ -13,20 +23,20 @@ module.exports = {
     });
 
     // changing status for the 5 minutes interval
-    setInterval(async () => {
-      const status = require("../assets/json/bot-status.json");
-      const randomStatus = status[Math.floor(Math.random() * status.length)];
+    // setInterval(async () => {
+    //   const status = require("../assets/json/bot-status.json");
+    //   const randomStatus = status[Math.floor(Math.random() * status.length)];
 
-      client.user.setPresence({
-        activities: [
-          {
-            name: randomStatus
-              .replace("{servers}", client.guilds.cache.size)
-              .replace("{totalStatuses}", status.length) + ` | @${client.user.username} help`,
-            type: "WATCHING",
-          },
-        ],
-      });
-    }, 300000);
+    //   client.user.setPresence({
+    //     activities: [
+    //       {
+    //         name: randomStatus
+    //           .replace("{servers}", client.guilds.cache.size)
+    //           .replace("{totalStatuses}", status.length) + ` | @${client.user.username} help`,
+    //         type: "WATCHING",
+    //       },
+    //     ],
+    //   });
+    // }, 300000);
   },
 };
