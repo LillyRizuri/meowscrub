04f5d25b717d70d3e74e73cda52720e34ef38733
scrubthispie <minh99460@gmail.com>
2021-09-18 15:43:29 +0700

    stuff

--- src/commands/funs/fight.js
+++ src/commands/funs/fight.js
@@ -33,8 +33,9 @@ module.exports = {
       message: message,
       opponent: message.mentions.users.first(),
       embed: {
-        title: "Fight!!!!!!",
         color: "#7289da",
+        title: "Fight",
+        footer: "Hmm...",
         timestamp: true,
       },
       buttons: {
@@ -45,17 +46,17 @@ module.exports = {
         deny: "Deny",
       },
       acceptMessage:
-        "<@{{challenger}}> has challenged <@{{opponent}}> for a fight!",
-      winMessage: "GG, <@{{winner}}> won the fight!",
+        `<@{{challenger}}> has challenged <@{{opponent}}> for a fight.\nDo you want to fight, ${target.toString()}?`,
+      winMessage: "<@{{winner}}> won the fight!",
       endMessage:
         "<@{{opponent}}> didn't answer in time. Game cancelled.",
-      cancelMessage: "<@{{opponent}}> refused to have a fight with you!",
-      fightMessage: "{{player}} you go first!",
+      cancelMessage: "<@{{opponent}}> backed off from the fight.",
+      fightMessage: "{{player}} you go first.",
       opponentsTurnMessage: "Please wait for your opponents move!",
       highHealthMessage: "You cannot heal if your HP is above 80!",
       lowHealthMessage: "You cannot cancel the fight if your HP is below 50!",
       returnWinner: false,
-      othersMessage: "Only {{author}} can use the buttons!",
+      othersMessage: "Only {{author}} can use the buttons.",
     });
   },
 };
