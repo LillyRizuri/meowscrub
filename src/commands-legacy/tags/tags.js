586df6c1bf0970204d5ae5660e9cbd12d8559d56
scrubthispie <minh99460@gmail.com>
2021-09-19 10:27:21 +0700

    modified "tags" command's output

--- src/commands-legacy/tags/tags.js
+++ src/commands-legacy/tags/tags.js
@@ -1,9 +1,4 @@
 const Discord = require("discord.js");
-const { pagination } = require("reconlx");
-
-const util = require("../../util/util");
-
-const emoji = require("../../assets/json/tick-emoji.json");
 
 const tagsSchema = require("../../models/tags-schema");
 
@@ -26,49 +21,14 @@ module.exports = {
     else
       tagsList = results.tags
         .sort((a, b) => a.name.localeCompare(b.name))
-        .map((tag, i) => `#${i + 1} • ${tag.name}`)
-        .join("\n");
+        .map((tag) => `\`${tag.name}\``)
+        .join(" ");
 
-    const listSplit = util.splitString(tagsList, 10);
+    const tagsEmbed = new Discord.MessageEmbed()
+      .setColor("RANDOM")
+      .setAuthor(`All tags in ${message.guild.name}`, message.guild.iconURL())
+      .setDescription(tagsList);
 
-    const embeds = [];
-    for (let i = 0; i < listSplit.length; i++) {
-      const tagsEmbed = new Discord.MessageEmbed()
-        .setColor("RANDOM")
-        .setTitle(`All tags in ${message.guild.name}`)
-        .setDescription(`\`\`\`css\n${listSplit[i]}\`\`\``)
-        .setFooter("tags good");
-      embeds.push(tagsEmbed);
-    }
-
-    pagination({
-      embeds: embeds,
-      author: message.author,
-      channel: message.channel,
-      fastSkip: true,
-      time: 60000,
-      button: [
-        {
-          name: "first",
-          emoji: emoji.firstEmoji,
-          style: "PRIMARY",
-        },
-        {
-          name: "previous",
-          emoji: emoji.leftEmoji,
-          style: "PRIMARY",
-        },
-        {
-          name: "next",
-          emoji: emoji.rightEmoji,
-          style: "PRIMARY",
-        },
-        {
-          name: "last",
-          emoji: emoji.lastEmoji,
-          style: "PRIMARY",
-        },
-      ],
-    });
+    message.channel.send({ embeds: [tagsEmbed] });
   },
 };
